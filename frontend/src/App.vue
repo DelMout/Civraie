92578f755996ca4fcf6ab58900797515e41be91a
Delphine Moutault <dlphn@hotmail.fr>
2021-07-20 17:52:44 +0200

    Install Vue CLI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<img alt="Vue logo" src="./assets/logo.png" />
-	<HelloWorld msg="Welcome to La Civraie !!! App" />
+	<HelloWorld msg="Welcome to La Civraie  App" />
 </template>
 
 <script>
